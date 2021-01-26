d0bec0c78613c836a1f4eb34061a15c7535a10d5
Miller Correa <macorreag@unal.edu.co>
2021-01-25 20:56:50 -0500

    define CLI help and simple test for CLI

---
+++ lib/issues/cli.ex
@@ -0,0 +1,70 @@
+defmodule Issues.CLI do
+  @default_count 4
+  @moduledoc """
+  Handle the command line parsing and the dispatch to
+  the various functions that end up generating a
+  table of the last _n_ issues in a github project
+  """
+  def run(argv) do
+    argv
+    |> parse_args
+    |> process
+  end
+
+  def process(:help) do
+    IO.puts("""
+    usage: issues <user> <project> [ count | #{@default_count} ]
+    """)
+
+    # Force to close app like return 0
+    System.halt(0)
+  end
+
+  def process({user, project, _count}) do
+    Issues.GithubIssues.fetch(user, project)
+  end
+
+  @doc """
+  `argv` can be -h or --help, which returns :help.
+  Otherwise it is a github user name, project name, and (optionally)
+  the number of entries to format.
+  Return a tuple of `{ user, project, count }`, or `:help` if help was given.
+  """
+  def parse_args(argv) do
+    OptionParser.parse(argv,
+      switches: [help: :boolean],
+      aliases: [h: :help]
+    )
+    # Extraer el primer elemento de la tupla
+    |> elem(1)
+    # Finalmente se inserta en la función creada
+    |> args_to_internal_representation()
+  end
+
+  # case parse do
+  #   {[help: true], _, _} ->
+  #     :help
+
+  #   {_, [user, project, count], _} ->
+  #     {user, project, String.to_integer(count)}
+
+  #   {_, [user, project], _} ->
+  #     {user, project, @default_count}
+
+  #   _ ->
+  #     :help
+  # end
+
+  def args_to_internal_representation([user, project, count]) do
+    {user, project, String.to_integer(count)}
+  end
+
+  def args_to_internal_representation([user, project]) do
+    {user, project, @default_count}
+  end
+
+  # bad arg or --help
+  def args_to_internal_representation(_) do
+    :help
+  end
+end
