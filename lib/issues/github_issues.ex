0e49009070c73da1098d8c14ae643a8fbfd04ae0
Miller Correa <macorreag@unal.edu.co>
2021-01-26 10:59:58 -0500

    FIX problems and make escript to build app

--- lib/issues/github_issues.ex
+++ lib/issues/github_issues.ex
@@ -11,19 +11,10 @@ defmodule Issues.GithubIssues do
     "#{@github_url}/repos/#{user}/#{project}/issues"
   end
 
-  def handle_response({:ok, %{status_code: 200, body: body}}) do
-    {:ok, body}
-  end
-
-  def handle_response({_, %{status_code: _, body: body}}) do
-    {:error, body}
-  end
-
   def handle_response({_, %{status_code: status_code, body: body}}) do
     {
       status_code |> check_for_error(),
-      body
-      |> Poison.Parser.parse!()
+      body |> Poison.Parser.parse!()
     }
   end
 
